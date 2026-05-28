ffba2be379af45d3e1d1f19214fb2517b4ca560f
Dakoda Bender <dakodabender49@gmail.com>
2026-05-27 21:50:58 -0400

    fix unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 
 const PLATFORMS = ["TikTok", "Instagram", "Google Trends", "Reddit", "X/Twitter"];
 
